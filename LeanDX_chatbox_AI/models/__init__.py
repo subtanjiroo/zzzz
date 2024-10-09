44e695398e220b14b5b3f69271be16e255de907d
subtanjiroo <138212507+subtanjiroo@users.noreply.github.com>
2024-10-09 11:58:31 +0700

    Delete LeanDX_chatbox_AI directory

--- LeanDX_chatbox_AI/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import chat_session
-from . import document_manager
-from . import res_config_settings
-from . import chat_box_ai
--- LeanDX_chatbox_AI/models/chat_box_ai.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-class Chatbox(models.TransientModel):
-    _name = 'leandx.ai.chatbox'
-    _description = 'Chat Message'
-
-    session = fields.Many2one('leandx.ai.session', string='Session')
-    name = fields.Char(string='Name')
-
